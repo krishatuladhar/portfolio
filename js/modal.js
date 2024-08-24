e24cac3e5ec5a8ca3a8ec721add73c2e1de63db4
krishatuladhar <krishatuladhar@gmail.com>
2024-08-24 14:56:36 +0545

    service-section

--- js/modal.js
+++ js/modal.js
@@ -17,3 +17,19 @@ workModalCloses.forEach((modalClose) => {
       .classList.remove("active-modal");
   });
 });
+
+/*===== SERVICES MODAL ===============*/
+const serviceModalBtns = document.querySelectorAll('.services__button');
+const serviceModalCloses = document.querySelectorAll('.services__modal-close');
+
+serviceModalBtns.forEach((modalBtn, index) => {
+    modalBtn.addEventListener('click', () => {
+        document.querySelectorAll('.services__modal')[index].classList.add('active-modal');
+    });
+});
+
+serviceModalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        document.querySelector('.services__modal.active-modal').classList.remove('active-modal');
+    });
+});
