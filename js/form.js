f839bf297ef6bbc2f1cfb2d255828a959544f0ea
krishatuladhar <krishatuladhar@gmail.com>
2024-08-24 23:33:33 +0545

    contact-section

---
+++ js/form.js
@@ -0,0 +1,5 @@
+/*===== CONTACT FORM SUBMISSION ===============*/
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+  alert('Thank you for your message! I will get back to you soon.');
+});
